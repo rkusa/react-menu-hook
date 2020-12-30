0c5b4389bd9edd8ac4825156ee1acaacfe96536b
Markus Ast <m@rkusa.st>
2020-12-30 14:53:13 +0100

    add test TODOs for the menu

--- test/useMenu.test.tsx
+++ test/useMenu.test.tsx
@@ -126,3 +126,81 @@ describe("Menu Button", () => {
     });
   });
 });
+
+// https://www.w3.org/TR/wai-aria-practices-1.2/#menu
+// The following tests ignore everything related to horizontal menus and submenus, it only assums
+// vertical one-level menus (for now).
+describe("Menu", () => {
+  // https://www.w3.org/TR/wai-aria-practices-1.2/#keyboard-interaction-12
+  describe("Keyboard Interaction", () => {
+    describe("Enter", () => {
+      test.todo(
+        "When focus is on a menuitemcheckbox, toggles the item and closes the menu"
+      );
+      test.todo(
+        "When focus is on a menuitemradio, toggles the item and closes the menu"
+      );
+      test.todo(
+        "When focus is on a menuitem, activates the item and closes the menu"
+      );
+    });
+
+    describe("Space", () => {
+      test.todo(
+        "When focus is on a menuitemcheckbox, changes the state without closing the menu"
+      );
+      test.todo(
+        "When focus is on a menuitemradio that is not checked, without closing the menu, checks the focused menuitemradio and unchecks any other checked menuitemradio element in the same group"
+      );
+      test.todo(
+        "When focus is on a menuitem, activates the menuitem and closes the menu"
+      );
+    });
+
+    describe("Down Arrow", () => {
+      test.todo("When focus is in a menu, moves focus to the next item");
+      test.todo("Wraps from the last to the first menuitem");
+    });
+
+    describe("Up Arrow", () => {
+      test.todo("When focus is in a menu, moves focus to the previous item");
+      test.todo("Wraps from the first to the last menuitem");
+    });
+
+    describe("Escape", () => {
+      test.todo(
+        "Close the menu that contains focus and return focus to the menu button from which the menu was opened"
+      );
+    });
+
+    describe("Tab", () => {
+      test.todo("Moves focus to the next element in the tab sequence");
+      test.todo("Closes the menu if element outside of the menu got focused");
+    });
+
+    describe("Shift + Tab", () => {
+      test.todo("Moves focus to the previous element in the tab sequence");
+      test.todo("Closes the menu if element outside of the menu got focused");
+    });
+
+    test.todo("Disabled menu items are focusable but cannot be activated");
+  });
+
+  // https://www.w3.org/TR/wai-aria-practices-1.2/#wai-aria-roles-states-and-properties-13
+  describe("WAI-ARIA Roles, States, and Properties", () => {
+    test.todo(
+      "The items contained in a menu are child elements of the containing menu and have any of the following roles: menuitem, menuitemcheckbox, menuitemradio"
+    );
+    test.todo("Each item in the menu has tabindex set to -1");
+    test.todo(
+      "When a menuitemcheckbox or menuitemradio is checked, aria-checked is set to true"
+    );
+    test.todo("When a menu item is disabled, aria-disabled is set to true.");
+    test.todo(
+      "All separators should have aria-orientation consistent with the separator's orientation."
+    );
+    test.todo(
+      "The menu has aria-labelledby set to a value that refers to the button that controls its display"
+    );
+  });
+});
